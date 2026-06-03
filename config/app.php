4ce808344be8a7b7db69d940b711789d87512db1
Rizki Pratama Sunarko <rizkypratama16322@gmail.com>
2026-06-03 09:28:55 +0700

    feat: implement dynamic BASEURL configuration and add digital signature asset

--- config/app.php
+++ config/app.php
@@ -1,4 +1,20 @@
 <?php
 
-// Pastikan slash (/) di akhir atau polanya sesuai dengan struktur routingmu
-define('BASEURL', 'http://localhost/Code_tugas/RPL/sigap-project/public');
+$scheme = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https' : 'http';
+$host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+$scriptName = $_SERVER['SCRIPT_NAME'] ?? '';
+$scriptDir = str_replace('\\', '/', dirname($scriptName));
+$scriptDir = rtrim($scriptDir, '/');
+
+
+$scriptFilename = str_replace('\\', '/', $_SERVER['SCRIPT_FILENAME'] ?? '');
+$isRootScript = (basename($scriptFilename) === 'index.php' && strpos($scriptFilename, '/public/') === false);
+
+if ($isRootScript) {
+    $basePath = $scriptDir . '/public';
+} else {
+    $basePath = $scriptDir;
+}
+
+$baseUrl = $scheme . '://' . $host . $basePath;
+define('BASEURL', rtrim($baseUrl, '/'));
